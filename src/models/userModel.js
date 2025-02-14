4dd9beef91b8e6893950875fdd48ee62b71cf6ea
Trishane Reid <trishanereid17@gmail.com>
2025-02-14 16:25:33 +0530

    hash user password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please enter the phone number'],
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
