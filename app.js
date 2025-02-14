d793237a910b684092d1702f6ee13afeceec0590
Trishane Reid <trishanereid17@gmail.com>
2025-02-14 14:38:35 +0530

    create auth/signup API & async error catch func

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const authRoutes = require('./src/routes/authRoutes');
 
 const app = express();
+
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
+
 module.exports = app;
